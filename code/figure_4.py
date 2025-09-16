af264f541e30325cc80d0f0016ee71b77949f0cc
Waschi97 <tom.waschischeck@web.de>
2025-09-16 17:04:18 +0200

    solving with solve_ivp, plotting all V4 in different colors

---
+++ code/figure_4.py
@@ -0,0 +1,40 @@
+from model import ode_model
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib as mpl
+
+# 10 different values of V4
+V4s = np.linspace(0.1, 10., 10)
+# one different color from a colormap for each V4
+cmap = mpl.colormaps['plasma']
+colors = cmap(np.linspace(0, 1, 10))
+
+P_0 = 0.25
+Q_0 = 0.9
+R_0 = 0.02
+
+param_dict = {
+    'a': 0.1,
+    'b': 0.1,
+    'K': 0.2,
+    'V1': 1.0,
+    'V2': 1.5,
+    'K1': 0.01,
+    'K2': 0.01,
+    'V3': 6.0,
+    'V4': 2.5,
+    'K3': 0.01,
+    'K4': 0.01
+}
+
+plt.figure()#figsize=(6,4))
+
+for i, V4 in enumerate(V4s):
+    param_dict['V4'] = V4
+    sol  = solve_ivp(ode_model, [0, 50], [P_0, Q_0, R_0], args=(param_dict,))
+    plt.plot(sol.t, sol.y[0], label = f'V4={np.round(V4,1)}', color=colors[i])
+
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.tight_layout()
+plt.savefig('figure_4_different_V4.png')
