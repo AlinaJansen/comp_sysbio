25d8d2daf6e44cfc797b5cdbf4b988f464dfa58c
Waschi97 <tom.waschischeck@web.de>
2025-09-16 17:34:31 +0200

    comments

--- code/figure_4.py
+++ code/figure_4.py
@@ -10,10 +10,12 @@ V4s = np.linspace(0.1, 10., 10)
 cmap = mpl.colormaps['plasma']
 colors = cmap(np.linspace(0, 1, 10))
 
+# Initial conditions
 P_0 = 0.25
 Q_0 = 0.9
 R_0 = 0.02
 
+# Parameters
 param_dict = {
     'a': 0.1,
     'b': 0.1,
@@ -30,11 +32,13 @@ param_dict = {
 
 plt.figure()
 
+# solve ODEs for each V4 and plot P over time
 for i, V4 in enumerate(V4s):
     param_dict['V4'] = V4
     sol  = solve_ivp(ode_model, [0, 140], [P_0, Q_0, R_0], method='RK23', args=(param_dict,),dense_output=True)
     plt.plot(sol.t, sol.y[0], label = f'V4={np.round(V4,1)}', color=colors[i])
 
+# big legend -> outside of plot
 plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.xlabel('Time')
 plt.ylabel('P', rotation=0)
