5e91e2c90d67e1c988944a4903b70403df883726
AlinaJansen <alina.jansen@fu-berlin.de>
2025-09-16 10:58:10 +0200

    added model with parameter dict

---
+++ code/model.py
@@ -0,0 +1,6 @@
+def rhs(t, y, param_dict):
+    P, Q, R = y
+    dP = param_dict['a']*Q - param_dict['b'] * P / (param_dict['K'] + P)
+    dQ = param_dict['V1'] * (1 - Q) / (param_dict['K1'] + (1 - Q)) - param_dict['V2'] * R * Q / (param_dict['K2'] + Q)
+    dR = param_dict['V3'] * P * (1 - R) / (param_dict['K3'] + (1 - R)) - param_dict['V4'] * R / (param_dict['K4'] + R)
+    return [dP, dQ, dR]
